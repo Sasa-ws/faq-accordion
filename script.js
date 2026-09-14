ac1c1ed094c743d573c082f74cd268447677c4fc
Sasa-ws <wsasa2205@gmail.com>
2026-09-14 03:33:28 -0700

    Update FAQ icon state

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ questions.forEach((question) => {
   question.addEventListener("click", () => {
     const answerId = question.getAttribute("aria-controls");
     const answer = document.getElementById(answerId);
+    const icon = question.querySelector(".faq-icon");
     const isExpanded = question.getAttribute("aria-expanded") === "true";
 
     question.setAttribute("aria-expanded", String(!isExpanded));
     answer.hidden = isExpanded;
+    icon.textContent = isExpanded ? "+" : "−";
   });
 });
